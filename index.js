0adfcb2ebfe3e53138cc396e5b0ed687f0bcc4d7
Petar Ostojic <pera.ostojic@gmail.com>
2021-01-25 12:53:44 +0100

    Add timestamp to random quizz

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ connection.query(dumpString, (err, res) => {
       });
     }
     if (command == "random") {
-      let randomPlaylistStmt = `SELECT * FROM song ORDER BY RAND() LIMIT 15`;
+      let randomPlaylistStmt = `SELECT * FROM song s, song_in_playlist sp WHERE s.id = sp.id_song ORDER BY RAND() LIMIT 15`;
       connection.query(randomPlaylistStmt, (err, res) => {
         if (err) {
           console.log("ERROR KOD RANDOMA:  ", err);
